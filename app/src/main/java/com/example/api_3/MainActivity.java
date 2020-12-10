58cd80ddd4e5837ae2f37dbab67db1af3b7250e2
syneart <sa@syneart.com>
2020-12-11 00:09:55 +0800

    Update# request GPS permission and check GPS status for TelephonyManager CellInfo

--- app/src/main/java/com/example/api_3/MainActivity.java
+++ app/src/main/java/com/example/api_3/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.api_3;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.telephony.CellInfo;
 import android.telephony.CellInfoLte;
-import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -18,7 +22,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView ss;
+    private static final String TAG = "MainActivity";
+    private static final int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 1;
+    private static final int MY_PERMISSIONS_GPS_SETTING_RETURN = 2;
+
+    private TextView tvDbmInfo;
     int dbm = -1;
 
     @Override
@@ -27,29 +35,72 @@ public class MainActivity extends AppCompatActivity {
 
         //setup content stuff
         this.setContentView(R.layout.activity_main);
-        ss = (TextView) findViewById(R.id.ss);
+        tvDbmInfo = (TextView) findViewById(R.id.ss);
 
-        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        List<CellInfo> cellInfoList = tm.getAllCellInfo();
-        for (CellInfo cellInfo : cellInfoList)
-        {
-            if (cellInfo instanceof CellInfoLte)
-            {
-                // cast to CellInfoLte and call all the CellInfoLte methods you need
-                dbm = ((CellInfoLte)cellInfo).getCellSignalStrength().getDbm();
+        getCellInfo();
+    }
 
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Permission Granted");
+            } else {
+                Toast.makeText(this, "Need GPS Permission", Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_ACCESS_FINE_LOCATION);
+                Log.d(TAG, "Permission Not Granted, requestPermissions");
             }
         }
-        ss.setText(dbm);
+    }
+
+    private boolean isGpsEnabled() {
+        LocationManager status = (LocationManager) (this.getSystemService(Context.LOCATION_SERVICE));
+        if (status.isProviderEnabled(LocationManager.GPS_PROVIDER)|| status.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            return true;
+        } else {
+            Toast.makeText(this, "Please enable GPS first", Toast.LENGTH_LONG).show();
+            startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), MY_PERMISSIONS_GPS_SETTING_RETURN);
+        }
+        return false;
+    }
+
+    private void getCellInfo() {
+        checkPermission();
+
+        if (isGpsEnabled()) {
+            TelephonyManager mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            List<CellInfo> cellInfoList = mTelephonyManager.getAllCellInfo();
+            Log.d(TAG, "cellInfoList.size = " + cellInfoList.size());
+            for (CellInfo cellInfo : cellInfoList) {
+                Log.d(TAG, "cellInfo = " + cellInfo.toString());
+                if (cellInfo instanceof CellInfoLte) {
+                    Log.d(TAG, "cellInfo(LTE) = " + cellInfo.toString());
+                    // cast to CellInfoLte and call all the CellInfoLte methods you need
+                    dbm = ((CellInfoLte) cellInfo).getCellSignalStrength().getDbm();
+                    tvDbmInfo.setText(String.valueOf(dbm));
+                }
+            }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: requestCode = " + requestCode + ", resultCode = " + resultCode);
+        if (resultCode == 0) {
+            switch (requestCode) {
+                case MY_PERMISSIONS_GPS_SETTING_RETURN:
+                    getCellInfo();
+                    break;
+            }
+        } else if (resultCode == 1) {
+            switch (requestCode) {
+                case MY_PERMISSIONS_ACCESS_FINE_LOCATION:
+                    checkPermission();
+                    break;
+            }
+        }
     }
 }
